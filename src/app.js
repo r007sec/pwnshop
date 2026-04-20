51b1ec48e8dcf60d035a3a5486c71b5ea97b5e65
Ibrahim Mutiu Olajide <r007user@gmail.com>
2026-04-20 17:30:41 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,10 +14,10 @@ const https = require('https');
 const app = express();
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'pwnshop'
+    host:     process.env.DB_HOST     || 'localhost',
+    user:     process.env.DB_USER     || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_NAME     || 'pwnshop'
 });
 
 db.connect((err) => {
